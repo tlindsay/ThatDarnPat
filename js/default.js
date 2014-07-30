ce5bf74823d662f95193f6717d4516b01b9d107f
Patrick Lindsay <tlindsay@una.edu>
2014-07-29 22:01:24 -0500

    Updating because I forgot I had this in git.

--- js/default.js
+++ js/default.js
@@ -5,3 +5,35 @@
 
 
 // JS functions and initiations go here...
+
+$(document).ready(function(){
+	var location = document.location.hash.substring(1);
+	if(location){
+		location = location.toLowerCase();
+		console.log(location);
+		switch(location){
+			case 'home':
+				home(); break;
+			case 'resume':
+				resume(); break;
+			default:
+				home();
+		}
+	}else{
+		home();
+	}
+});
+
+function clearContent(){
+	$('#content').html('');
+}
+
+function home(){
+	clearContent();
+	$('#content').load('assets/html/home.html#content');
+}
+
+function resume(){
+	clearContent();
+	$('#content').load('assets/html/resume.html');
+}
