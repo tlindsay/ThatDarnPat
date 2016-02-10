c6ba73e03f964c503bc3986474a11e6c3ff3da9f
Patrick Lindsay <tlindsay@una.edu>
2016-02-09 22:10:41 -0600

    Contact form wired up and ready to go

--- source/javascripts/contact.js
+++ source/javascripts/contact.js
@@ -1,4 +1,30 @@
-$('#contact-form').on('submit', function(e) {
-  e.preventDefault();
-  console.log('yay');
+$(document).ready(function() {
+  $('#contact-form').on('submit', function(e) {
+    e.preventDefault();
+    f = $(this);
+    n = f.find('input[name="name"]').val();
+    e = f.find('input[type="email"]').val();
+    m = f.find('textarea').val();
+    if(grecaptcha.getResponse() !== '')
+      $.ajax({
+        url: "//formspree.io/patrick.lindsay@me.com", 
+        method: "POST",
+        data: {
+          name: n,
+          email: e,
+          message: m
+        },
+        dataType: "json",
+        success: function() {
+          f.text('');
+          $('.success-message').html('<h1>Message received!</h1><h3>I\'ll get back to you shortly.</h3>');
+        },
+        error: function() {
+
+        }
+      });
+    else{
+      $('.g-recaptcha').addClass('shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {$(this).removeClass('shake');});
+    }
+  });
 });
