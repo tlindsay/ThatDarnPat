f15a7d99e2de2533b8df0dedb79d60cc0fe9e761
Patrick Lindsay <tlindsay@una.edu>
2016-09-09 15:15:31 -0500

    Update UI on Projects page to be more intuitive

--- source/javascripts/projects.js
+++ source/javascripts/projects.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-  $('.project').click(function() {
-    $(this).toggleClass('active');
+  $('.project').click(function(e) {
+    $this = $(this);
+
+    if($(e.target).is(".body *"))
+      return;
+
+    if($this.hasClass('active'))
+      $('.active').removeClass('active');
+    else {
+      $('.active').removeClass('active');
+      $(this).addClass('active');
+    }
   });
 });
