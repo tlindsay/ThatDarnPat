7cc8255e1f2e0695420e67f1d00a870a4523032e
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-10-23 14:56:45 -0500

    Delete some default stuff and reformat some other default stuff

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      color: `white`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -20,8 +20,8 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            color: 'white',
+            textDecoration: `none`
           }}
         >
           {siteTitle}
@@ -29,14 +29,14 @@ const Header = ({ siteTitle }) => (
       </h1>
     </div>
   </header>
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
