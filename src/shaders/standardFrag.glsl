1dbe0241602ee72d5d83490bc41b7c56361f268c
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-10-30 13:47:51 -0500

    Standardize standardFrag a little more

--- src/shaders/standardFrag.glsl
+++ src/shaders/standardFrag.glsl
@@ -1,9 +1,6 @@
 precision highp float;
-uniform vec2 u_resolution;
-uniform float u_time;
-uniform float u_clock;
+varying vec2 vUv;
 
 void main() {
-  vec2 st = gl_FragCoord.xy / u_resolution;
-  gl_FragColor = vec4(vec3(st.y * u_clock), 1.0);
+  gl_FragColor = vec4(vec3(vUv.x), 1.0);
 }
